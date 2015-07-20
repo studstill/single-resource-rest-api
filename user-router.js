65f10dbd5f6986ddbd8c986abb0521fe8e00dddb
Jason Studstill <standstill64@gmail.com>
2015-07-19 20:31:57 -0700

    Implement request handling for GET (by id), POST, PUT DELET

--- user-router.js
+++ user-router.js
@@ -1,13 +1,93 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
 var bodyParser = require('body-parser');
 var User = require('./user-model');
 
-router.get('/', function(req, res) {
-  res.json({msg: 'GET request received at path "/"'});
-});
+module.exports = function(router) {
+  router.use(bodyParser.json());
 
-module.exports = router;
+/************************************************************
+/   Handle GET requests
+/************************************************************/
 
+  // Handle Root URL GET request
+  router.get('/', function(req, res) {
+    // Serve the entire users database
+    User.find({}, function(err, data) {
+      if (err)
+        res.status(400).json({msg: 'Error finding data'});
+      else
+        res.json(data);
+    });
+  });
 
+  // Handle ANY OTHER GET Request
+  router.get('/:id', function(req, res) {
+    var user = req.params.id;
+    console.log('Get User: ', user);
+    // Return document that matches username
+    User.find({username: user}, function(err, data) {
+      if (!err && data[0]) {
+        res.json(data);
+      } else {
+        res.status(404).json({error: 'No record found for ' + user});
+      }
+    });
+  });
 
+  /************************************************************
+  /   Handle POST requests
+  /************************************************************/
+
+  router.post('/', function(req, res) {
+    var newUser = new User(req.body);
+    // Unsure that data exists
+    // source: http://stackoverflow.com/questions/4994201/is-object-empty#answer-4994244
+    if (Object.getOwnPropertyNames(req.body).length > 0) {
+      newUser.save(function(err, newUser) {
+        if (err) return console.err(err);
+        res.json(newUser);
+      });
+    } else {
+      res.json({error: 'Input does not match schema',
+        userSchema: {'username': 'String', 'email': 'String', 'age': 'Number'}
+      });
+    }
+  });
+
+  /************************************************************
+  /   Handle PUT requests
+  /************************************************************/
+
+  router.put('/:id', function(req, res) {
+    var user = req.params.id;
+
+    User.update({username: user}, {$set: req.body}, {upsert: true},
+      function(err, updateMessageObj) {
+      if (!err) {
+        res.json({msg: 'Successfully created or updated username "' + user +
+          '"" with ' + JSON.stringify(req.body)});
+      } else {
+        res.status(404).json({error: 'Input does not match schema',
+          userSchema: {'username': 'String', 'email': 'String', 'age': 'Number'}
+        });
+      }
+    });
+  });
+
+  /************************************************************
+  /   Handle DELETE requests
+  /************************************************************/
+
+  router.delete('/:id', function(req, res) {
+    var user = req.params.id;
+    User.remove({username: user}, function(err, data) {
+      if (!err) {
+        res.json({msg: 'username: "' + user + '" has been deleted or no ' +
+          'longer exists'});
+      } else {
+        res.status(404).json({error: 'No record found for ' + user});
+      }
+    });
+  });
+};

e083aa3a64a25f30d26c495b7c93afd44ea4d904
Jason Studstill <standstill64@gmail.com>
2015-07-19 20:28:05 -0700

    Create catchAllMessage. Set up router and mongoose by following Tyler's examples.

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var userRouter = express.Router();
 
-module.exports = function startServer() {
-  var app = express(),
-    userRouter = require('./user-router'),
-    port = process.env.PORT || 3000;
+module.exports = function server() {
+
+  mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users');
+
+  require('./user-router')(userRouter);
+
+  app.use('/users', userRouter);
+
+  catchAllMessage = {msg: 'Please direct your URI to a valid endpoint.',
+    endpoints: ['/users']};
+
+  app.all(['/', '/index'], function(req, res) {
+    res.json(catchAllMessage);
+  });
+
+  app.all('*', function(req, res) {
+    catchAllMessage.error = '404 Not found';
+    res.status(404).json(catchAllMessage);
+  });
 
   app.listen(port);
-  app.use('/', userRouter);
   console.log('Server started on ' + port);
 };
 

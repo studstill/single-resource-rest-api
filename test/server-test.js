b2ddef3b2296b60bd50f1c00c588752617546483
Jason Studstill <standstill64@gmail.com>
2015-07-20 11:57:54 -0700

    Implement entire functionality of test file.  Test ensures that the server handles GET, POST, PUT, and DELETE

--- test/server-test.js
+++ test/server-test.js
@@ -1,15 +1,42 @@
+process.env.MONGOLAB_URI = 'mongodb://localhost/users_test';
+
+var mongoose = require('mongoose');
 var chai = require('chai');
 var chaiHTTP = require('chai-http');
 var expect = require('chai').expect;
 var startServer = require('../server.js');
+var User = require('../user-model');
 
 chai.use(chaiHTTP);
 
-// ***** START SERVER FOR TESTING ******* //
-startServer();
+
+// Create variables for testing
+var correctSchemaObj = {'username': 'jason123',
+                        'email': 'jason123@email.com',
+                        'age': 31};
+
+var incorrectSchemaObj = {'email': 'crazy_person123@aol.com',
+                          'favoriteActor': 'Tom Cruise'};
+
+var propToUpdate = {age: 5000};
 
 describe('server', function() {
-  it('Should respond to a "/" GET request', function(done) {
+
+  before(function() {
+    startServer();
+  });
+
+  after(function(done) {
+    mongoose.connection.db.dropDatabase();
+    mongoose.disconnect();
+    done();
+  });
+
+/************************************************************
+/   Handle GET requests
+/************************************************************/
+
+  it('Should respond to GET request to "/"', function(done) {
     chai.request('http://localhost:3000')
         .get('/')
         .then(function(res) {
@@ -18,4 +45,75 @@ describe('server', function() {
           done();
         });
   });
+
+  it('Should respond to GET request to "/users"',
+    function(done) {
+      chai.request('http://localhost:3000')
+        .get('/users')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(typeof(res.body)).to.eql('object');
+          done();
+        });
+  });
+
+  /************************************************************
+  /   Handle POST requests
+  /************************************************************/
+
+  it('Should respond to POST request to "/users"', function(done) {
+    chai.request('http://localhost:3000')
+      .post('/users')
+      .send(correctSchemaObj)
+      .then(function(res) {
+        expect(res.body.username).to.eql(correctSchemaObj.username);
+        expect(res.body.email).to.eql(correctSchemaObj.email);
+        expect(res.body.age).to.eql(correctSchemaObj.age);
+      });
+    chai.request('http://localhost:3000')
+      .post('/users')
+      .send(incorrectSchemaObj)
+      .then(function(res) {
+        expect(res.body.error).to.not.eql(null);
+        done();
+      });
+  });
+
+  /************************************************************
+  /   Handle PUT requests
+  /************************************************************/
+
+  it('Should respond to PUT request to "/users/id:"', function(done) {
+
+    chai.request('http://localhost:3000')
+      .put('/users/jason123')
+      .send(propToUpdate)
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.body).to.not.eql(undefined);
+        expect(res.body.ok).to.eql(1);
+        done();
+      });
+  });
+
+  /************************************************************
+  /   Handle DELETE requests
+  /************************************************************/
+
+  it('Should respond to DELETE request to "/users/id:"', function(done) {
+    chai.request('http://localhost:3000')
+      .del('/users/jason123')
+      .end(function(err, res) {
+      });
+
+    // Do GET request for deleted user to make sure it is gone
+    chai.request('http://localhost:3000')
+      .get('/users/jason123')
+      .then(function(res) {
+        // There SHOULD be an error
+        expect(res.error).to.not.eql(null);
+        done();
+      });
+  });
+
 });

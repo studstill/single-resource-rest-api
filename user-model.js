0f128d384a0e884261a86ffbf8ec327da9d6a699
Jason Studstill <standstill64@gmail.com>
2015-07-19 20:31:06 -0700

    Simplify user-model

--- user-model.js
+++ user-model.js
@@ -1,18 +1,11 @@
 'use strict';
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/users');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection:error'));
-
-db.once('open', function() {
-
-  var userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-    age: Number
-  });
-
-  module.exports = mongoose.model('User', userSchema);
+var userSchema = mongoose.Schema({
+  username: String,
+  email: String,
+  age: Number
 });
+
+module.exports = mongoose.model('User', userSchema);

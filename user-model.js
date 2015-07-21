ddf39861551096ed3e51d1af3e723ea7d51df94e
Jason Studstill <standstill64@gmail.com>
2015-07-20 21:41:54 -0700

    Implement validation for email addresses

--- user-model.js
+++ user-model.js
@@ -2,9 +2,29 @@
 
 var mongoose = require('mongoose');
 
+// Citation: validateEmail function and reqex come from
+// http://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax#answer-24214767
+
+var validateEmail = function(email) {
+    var emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegEx.test(email);
+};
+
 var userSchema = mongoose.Schema({
-  username: String,
-  email: String,
+  username: {
+    type: String,
+    unique: true,
+    required: 'Username is required'
+  },
+  email: {
+    type: String,
+    validate: [function(email) {
+      var emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return emailRegEx.test(email);
+      }, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    'Please fill a valid email address']
+  },
   age: Number
 });
 

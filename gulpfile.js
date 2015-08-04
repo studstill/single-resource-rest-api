017d6a69b8ff3c67bac613e02c01c23981f45c6b
Jason Studstill <standstill64@gmail.com>
2015-08-04 11:06:43 -0700

    Add copy, webpack, and ultimately build tasks.  Comment out the task to start mongod and change default to only build

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,20 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var exec = require('child_process').exec;
+var webpack = require('gulp-webpack');
 
-gulp.task('default', ['mongod', 'test', 'lint'], function() {});
+gulp.task('default', ['build'], function() {});
 
 // Actually start server by entering "mongod" in the command line
-gulp.task('mongod', function() {
-  exec('mongod', function(err, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (err !== null) {
-      console.log('exec error: ' + err);
-    }
-  });
-});
+// gulp.task('mongod', function() {
+//   exec('mongod', function(err, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
+//     if (err !== null) {
+//       console.log('exec error: ' + err);
+//     }
+//   });
+// });
 
 gulp.task('test', function() {
   return gulp.src('test/*test.js')
@@ -26,4 +27,23 @@ gulp.task('lint', function() {
             .pipe(jshint())
             .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('webpack', function() {
+  return gulp.src('app/js/client.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['webpack', 'copy']);
+
+gulp.watch(['app/js/*.js', 'app/*.html'], ['copy', 'webpack'])
 

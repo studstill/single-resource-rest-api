ffb58e04d926f0b8c1b70bbe479dad005a410c8c
Jason Studstill <standstill64@gmail.com>
2015-08-04 11:07:54 -0700

    Update server file to account for moved router and comment out the catch-all routes

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var userRouter = express.Router();
 
-
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users');
 
-require('./user-router')(userRouter);
+require('./routes/user-router')(userRouter);
 
+app.use(express.static(__dirname + '/build'));
 app.use('/users', userRouter);
 
 catchAllMessage = {msg: 'Please direct your URI to a valid endpoint.',
   endpoints: ['/users']};
 
-app.all(['/', '/index'], function(req, res) {
-  res.json(catchAllMessage);
-});
+// app.all(['/', '/index'], function(req, res) {
+//   res.json(catchAllMessage);
+// });
 
-app.all('*', function(req, res) {
-  catchAllMessage.error = '404 Not found';
-  res.status(404).json(catchAllMessage);
-});
+// app.all('*', function(req, res) {
+//   catchAllMessage.error = '404 Not found';
+//   res.status(404).json(catchAllMessage);
+// });
 
 app.listen(port, function() {
   console.log('Server started on ' + port);

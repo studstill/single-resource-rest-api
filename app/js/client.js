9f8d4b0f64fee78e521ac587e41040723cf31452
Jason Studstill <standstill64@gmail.com>
2015-08-06 09:48:04 -0700

    simplified client.js

--- app/js/client.js
+++ app/js/client.js
@@ -2,17 +2,7 @@
 
 require('angular/angular');
 
-var notesApp = angular.module('notesApp', []);
+var usersApp = angular.module('usersApp', []);
 
-var notesController = notesApp.controller('notesController', ['$scope', function($scope) {
-  $scope.username = 'Your Username';
-  $scope.appName = 'Virtual Trapper Keeper';
-  $scope.appDescription = 'This app saves notes for a specific username';
-  $scope.greeting = 'hello world';
-  $scope.saveNote = function() {
-    alert('In the final version of this program, your note will saved to a ' +
-    'database: \n\n' + '{"username":"' + $scope.username + '", "note":"' +
-    $scope.appDescription + '"}');
-  };
+require('./users/users')(usersApp);
 
-}]);

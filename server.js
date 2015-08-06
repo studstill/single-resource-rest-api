8c2fb02e6c3fcd339f04b7df150a801e7c9c00df
Jason Studstill <standstill64@gmail.com>
2015-08-06 09:42:49 -0700

    Simply server

--- server.js
+++ server.js
@@ -9,19 +9,11 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users');
 require('./routes/user-router')(userRouter);
 
 app.use(express.static(__dirname + '/build'));
-app.use('/users', userRouter);
+app.use('/api', userRouter);
 
-catchAllMessage = {msg: 'Please direct your URI to a valid endpoint.',
-  endpoints: ['/users']};
-
-// app.all(['/', '/index'], function(req, res) {
-//   res.json(catchAllMessage);
-// });
-
-// app.all('*', function(req, res) {
-//   catchAllMessage.error = '404 Not found';
-//   res.status(404).json(catchAllMessage);
-// });
+app.all('*', function(req, res) {
+  res.status(404).send('404 Not Found');
+});
 
 app.listen(port, function() {
   console.log('Server started on ' + port);

d6691082160b407e1ad77b0448e0f6fcf6e62d62
Jason Studstill <standstill64@gmail.com>
2015-08-06 09:44:57 -0700

    Updated user-model to remove "age" property and saved it in a models directory

--- models/user-model.js
+++ models/user-model.js
@@ -2,13 +2,30 @@
 
 var mongoose = require('mongoose');
 
+// Citation: validateEmail function and reqex come from
+// http://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax#answer-24214767
+
+var validateEmail = function(email) {
+    var emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegEx.test(email);
+};
+
 var userSchema = mongoose.Schema({
   username: {
     type: String,
     unique: true,
     required: 'Username is required'
   },
-  note: String
+  email: {
+    type: String,
+    validate: [function(email) {
+      var emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return emailRegEx.test(email);
+      }, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    'Please fill a valid email address']
+  },
+  age: Number
 });
 
 module.exports = mongoose.model('User', userSchema);
